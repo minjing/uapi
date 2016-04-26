836e626c62f699bb5ec1a0d287345cc6d8438d12
xquan <xiaoming.quan@tekcomms.com>
2016-04-26 16:47:19 +0800

    Add new project for remoting service loading

---
+++ uapi.service.remote/src/main/java/uapi/service/remote/internal/RemoteServiceLoader.java
@@ -0,0 +1,19 @@
+package uapi.service.remote.internal;
+
+import uapi.service.IServiceLoader;
+
+/**
+ * ServiceLoader used to load service remotely
+ */
+public class RemoteServiceLoader implements IServiceLoader {
+    
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public <T> T load(String serviceId) {
+        return null;
+    }
+}
