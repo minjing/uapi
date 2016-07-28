cece72bdd70c9ee99a5e402a020ce2322703f49e
minjing <min_jing@aliyun.com>
2016-07-28 10:23:29 +0800

    Add original tests for kernal.service back

---
+++ uapi.kernel/src/test/groovy/uapi/service/internal/RegistryTest.groovy
@@ -0,0 +1,173 @@
+/**
+ * Copyright (C) 2010 The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.service.internal
+
+import spock.lang.Specification
+import uapi.service.Dependency
+import uapi.service.IInjectable
+import uapi.service.IRegistry
+import uapi.service.ISatisfyHook
+import uapi.service.IService
+import uapi.service.IServiceLoader
+import uapi.service.Injection
+
+/**
+ * Test case for Registry
+ */
+class RegistryTest extends Specification {
+
+    Registry registry;
+
+    def setup() {
+        registry = new Registry()
+    }
+
+    def "Test get id"() {
+        expect:
+        registry.getIds() == [IRegistry.canonicalName] as String[]
+    }
+
+    def "Register a normal service with id"() {
+        when:
+        registry.register(service, serviceId)
+
+        then:
+        registry.findService(serviceId) == service
+
+        where:
+        serviceId | service
+        "1"       | Mock(Object)
+    }
+
+    def "Register a IService instance with id"() {
+        def svc = Mock(IService) {
+            getIds() >> ["1", "2"]
+        }
+
+        when:
+        registry.register(svc)
+
+        then:
+        registry.findService("1") == svc
+        registry.findService("2") == svc
+        registry.getCount() == 2
+    }
+
+    def "Register more IService instances"() {
+        def svc1 = Mock(IService) {
+            getIds() >> ["1", "2"]
+        }
+        def svc2 = Mock(IService) {
+            getIds() >> ["3", "4"]
+        }
+
+        when:
+        registry.register(svc1, svc2)
+
+        then:
+        registry.findService("1") == svc1
+        registry.findService("2") == svc1
+        registry.findService("3") == svc2
+        registry.findService("4") == svc2
+        registry.getCount() == 4
+    }
+
+    def "Test Optional"() {
+        expect:
+        registry.isOptional(svcId) == optional
+
+        where:
+        svcId                       | optional
+        ISatisfyHook.canonicalName  | true
+    }
+
+    def "Test Satisfy Invocation"() {
+        def svc1 = Mock(IService) {
+            getIds() >> ["1", "2"]
+        }
+        def svc2 = Mock(IService) {
+            getIds() >> ["3", "4"]
+        }
+
+        given:
+        ISatisfyHook hook = Mock(ISatisfyHook) {
+            isSatisfied(_) >> true
+        }
+        Injection injection = Mock(Injection) {
+            getId() >> ISatisfyHook.canonicalName
+            getObject() >> hook
+        }
+        registry.injectObject(injection)
+
+        when:
+        registry.register(svc1, svc2)
+
+        then:
+        registry.findService("1") == svc1
+        registry.findService("2") == svc1
+        registry.findService("3") == svc2
+        registry.findService("4") == svc2
+        registry.getCount() == 4
+    }
+
+    def 'Test find service by id and from'() {
+        def svc1 = Mock(IService) {
+            getIds() >> ["1", "2"]
+        }
+        def svc2 = Mock(IService) {
+            getIds() >> ["3", "4"]
+        }
+
+        given:
+        ISatisfyHook hook = Mock(ISatisfyHook) {
+            isSatisfied(_) >> true
+        }
+        Injection injection = Mock(Injection) {
+            getId() >> ISatisfyHook.canonicalName
+            getObject() >> hook
+        }
+        registry.injectObject(injection)
+
+        when:
+        registry.register(svc1, svc2)
+
+        then:
+        registry.findService("1", QualifiedServiceId.FROM_LOCAL) == svc1
+        registry.findService("2", QualifiedServiceId.FROM_LOCAL) == svc1
+        registry.findService("3", QualifiedServiceId.FROM_LOCAL) == svc2
+        registry.findService("4", QualifiedServiceId.FROM_LOCAL) == svc2
+        registry.getCount() == 4
+    }
+
+    static interface IInjectableService extends IService, IInjectable {}
+
+//    def 'Test getUnresolvedServices method'() {
+//        def svc = Mock(IInjectableService) {
+//            getIds() >> ["1"]
+//            getDependencies() >> [dependQSvcId]
+//        }
+//        def svcLoader = Mock(IServiceLoader) {
+//            getId() >> "Test"
+//            load(dependSvcId, Object.class) >> dependSvc
+//        }
+//
+//        when:
+//        registry.register(svc)
+//        registry.registerServiceLoader(svcLoader)
+//
+//        then:
+//        1 * svcLoader.load(dependSvcId, Object.class)
+//
+//        where:
+//        dependSvcId | dependQSvcId                              | dependSvc
+//        'd1'        | new Dependency('d1@Test', Object.class)   | 'abc'
+//        'd2'        | new Dependency('d2@Any', Object.class)    | 'abc'
+//    }
+}
---
+++ uapi.kernel/src/test/groovy/uapi/service/internal/ServiceHolderTest.groovy
@@ -0,0 +1,186 @@
+/**
+ * Copyright (C) 2010 The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.service.internal
+
+import spock.lang.Ignore
+import spock.lang.Specification
+import sun.rmi.server.InactiveGroupException
+import uapi.service.Dependency
+import uapi.service.IInitial
+import uapi.service.IInjectable
+import uapi.service.IRegistry
+import uapi.service.ISatisfyHook
+import uapi.service.IService
+import uapi.service.IServiceFactory
+
+/**
+ * Test case for ServiceHolder
+ */
+class ServiceHolderTest extends Specification {
+
+    def "Test add a service"() {
+        given:
+        ISatisfyHook mockHook = Mock(ISatisfyHook)
+        ServiceHolder holder = new ServiceHolder(from, service, serviceId, mockHook)
+        mockHook.isSatisfied(service) >> true
+
+        expect:
+        holder.id == serviceId
+        holder.service == service
+        holder.inited == inited
+
+        where:
+        serviceId   | from                          | service           | resolved  | inited
+        "1"         | QualifiedServiceId.FROM_LOCAL | Mock(Object)      | true      | false
+        "2"         | QualifiedServiceId.FROM_LOCAL | Mock(IService)    | true      | false
+    }
+
+    def "Test init service on a normal service"() {
+        given:
+        ISatisfyHook mockHook = Mock(ISatisfyHook) {
+            isSatisfied(_) >> true
+        }
+        ServiceHolder holder = new ServiceHolder(from, service, serviceId, mockHook)
+
+        when:
+        holder.tryInitService()
+
+        then:
+        holder.id == serviceId
+        holder.service == service
+        holder.inited == inited
+
+        where:
+        serviceId   | from                          | service           | resolved  | inited
+        "1"         | QualifiedServiceId.FROM_LOCAL | Mock(Object)      | true      | true
+        "2"         | QualifiedServiceId.FROM_LOCAL | Mock(IService)    | true      | true
+    }
+
+    def "Test init service which is IInitial instance"() {
+        given:
+        IInitial initialSvc = Mock(IInitial)
+        ISatisfyHook mockHook = Mock(ISatisfyHook) {
+            isSatisfied(_) >> true;
+        }
+        ServiceHolder holder = new ServiceHolder(from, initialSvc, serviceId, mockHook)
+
+        when:
+        holder.id == serviceId
+        holder.service == initialSvc
+        holder.tryInitService()
+
+        then:
+        1 * initialSvc.init()
+
+        where:
+        serviceId   | from                          | resolved  | inited
+        "3"         | QualifiedServiceId.FROM_LOCAL | true      | true
+    }
+
+    def "Test service with dependency"() {
+        given:
+        IInjectable injectableSvc = Mock(IInjectable)
+        ISatisfyHook mockHook = Mock(ISatisfyHook)
+        ServiceHolder holder = new ServiceHolder(from, injectableSvc, serviceId, [new Dependency("dep01@Local", Object.class)] as Dependency[], mockHook)
+        mockHook.isSatisfied(injectableSvc) >> true
+
+        when:
+        holder.isDependsOn("dep01@Local")
+
+        then:
+        holder.id == serviceId
+
+        where:
+        serviceId   | from                          | resolved  | inited    | dependId
+        "1"         | QualifiedServiceId.FROM_LOCAL | false     | false     | "dep01"
+    }
+
+    @Ignore
+    def "Test set service dependency"() {
+        given:
+        IInjectable injectableSvc = Mock(IInjectable)
+        ServiceHolder dependSvc = Mock(ServiceHolder) {
+            getId() >> 'dep01'
+            getQualifiedId() >> QualifiedServiceId.splitTo('dep01@Local')
+            getService() >> new Object()
+            tryInitService() >> true
+        }
+        ISatisfyHook mockHook = Mock(ISatisfyHook) {
+            isSatisfied(_) >> true
+        }
+        ServiceHolder holder = new ServiceHolder(from, injectableSvc, serviceId, [new Dependency("dep01@Local", Object.class)] as Dependency[], mockHook)
+
+        when:
+        holder.setDependency(dependSvc)
+        holder.tryInitService()
+
+        then:
+        holder.id == serviceId
+        holder.inited == inited
+        1 * injectableSvc.injectObject(_)
+
+        where:
+        serviceId   | from                          | resolved  | inited
+        "1"         | QualifiedServiceId.FROM_LOCAL | true      | true
+    }
+
+    @Ignore
+    def "Test depends on a service factory"() {
+        given:
+        IInjectable injectableSvc = Mock(IInjectable)
+        IServiceFactory svcFactory = Mock(IServiceFactory) {
+            createService(injectableSvc) >> new Object()
+        }
+        ServiceHolder dependSvc = Mock(ServiceHolder) {
+            getId() >> 'dep01'
+            getQualifiedId() >> QualifiedServiceId.splitTo('dep01@Local')
+            getService() >> svcFactory
+            tryInitService() >> true
+        }
+        ISatisfyHook mockHook = Mock(ISatisfyHook) {
+            isSatisfied(_) >> true
+        }
+        ServiceHolder holder = new ServiceHolder(from, injectableSvc, serviceId, [new Dependency("dep01@Local", Object.class)] as Dependency[], mockHook)
+
+        when:
+        holder.setDependency(dependSvc)
+        holder.tryInitService()
+
+        then:
+        holder.id == serviceId
+        holder.inited == inited
+        1 * injectableSvc.injectObject(_)
+
+        where:
+        serviceId   | from                          | resolved  | inited
+        "1"         | QualifiedServiceId.FROM_LOCAL | true      | true
+    }
+
+    def "Test getUnresolvedServices method"() {
+        given:
+        IInjectable injectableSvc = Mock(IInjectable)
+        ISatisfyHook mockHook = Mock(ISatisfyHook) {
+            isSatisfied(_) >> true
+        }
+        ServiceHolder holder = new ServiceHolder(from, injectableSvc, serviceId, [new Dependency("dep01@Local", Object.class)] as Dependency[], mockHook)
+
+        when:
+        holder.tryInitService()
+
+        then:
+        holder.id == serviceId
+        holder.inited == inited
+//        holder.getUnresolvedServices('Local') == ['dep01']
+
+        where:
+        serviceId   | from                          | inited
+        "1"         | QualifiedServiceId.FROM_LOCAL | false
+    }
+}
