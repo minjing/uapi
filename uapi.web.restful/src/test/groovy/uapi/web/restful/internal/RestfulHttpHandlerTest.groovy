caf1d7d726280cff1a06efc8e354f23ca4971ae1
xquan <xiaoming.quan@netscout.com>
2016-09-02 17:25:02 +0800

    Add tests and remove uapi.web project

--- uapi.web.restful/src/test/groovy/uapi/web/restful/internal/RestfulHttpHandlerTest.groovy
+++ uapi.web.restful/src/test/groovy/uapi/web/restful/internal/RestfulHttpHandlerTest.groovy
@@ -14,7 +14,9 @@ import uapi.service.ArgumentMeta
 import uapi.service.IStringCodec
 import uapi.service.ServiceMeta
 import uapi.service.TypeMapper
+import uapi.web.restful.Constant
 import uapi.web.restful.IRestfulService
+import uapi.web.restful.IndexedArgumentMapping
 import uapi.web.restful.NamedArgumentMapping
 import uapi.web.http.HttpMethod
 import uapi.web.http.IHttpRequest
@@ -26,6 +28,21 @@ import uapi.web.restful.IRestfulInterface
  * Test case for RestfulHttpHandler
  */
 class RestfulHttpHandlerTest extends Specification {
+
+    def 'Test getUriMapping'() {
+        given:
+        RestfulHttpHandler handler = new RestfulHttpHandler();
+
+        when:
+        handler._context = modifiedContext
+
+        then:
+        handler.getUriMapping() == modifiedContext
+
+        where:
+        defaultContext                      | modifiedContext
+        Constant.DEF_RESTFUL_URI_PREFIX     | '/abc/restful'
+    }
 
     def 'Test discovery request'() {
         def namedArg = Mock(NamedArgumentMapping) {
@@ -112,5 +129,118 @@ class RestfulHttpHandlerTest extends Specification {
         where:
         host        | port  | codec     | svcId     | paramName | paramValue    | result
         'localhost' | 8080  | 'JSON'    | 'hello'   | 'title'   | 'Mr'          | 'Hello'
+    }
+
+    def 'Test request parameter from'() {
+        given:
+        def svc = Mock(IRestfulService) {
+            getMethodArgumentsInfo(HttpMethod.PUT) >> [Mock(NamedArgumentMapping) {
+                getName() >> paramName
+                getFrom() >> ArgumentFrom.Param
+                getType() >> String.class.getName()
+            }, Mock(IndexedArgumentMapping) {
+                getFrom() >> ArgumentFrom.Uri
+                getType() >> String.class.getName()
+                getIndex() >> 0
+            }, Mock(NamedArgumentMapping) {
+                getFrom() >> ArgumentFrom.Header
+                getType() >> String.class.getName()
+                getName() >> headerParamName
+            }]
+            invoke(HttpMethod.PUT, paramValue, 'aaa', headerParamValue) >> result
+        }
+        def request = Mock(IHttpRequest) {
+            method() >> HttpMethod.PUT
+            uri() >> '/rest/' + svcId + '/' + uriParam + '?' + paramName + '=' + paramValue
+            params() >> [ 'title' : [paramValue] as List ]
+            headers() >> [ headerParamName: headerParamValue ]
+        }
+        def response = Mock(IHttpResponse) {
+            1 * write(_)
+            1 * flush()
+        }
+
+        when:
+        RestfulHttpHandler restfulHandler = new RestfulHttpHandler()
+        restfulHandler._host = host
+        restfulHandler._port = port
+        restfulHandler._codecName = codec
+        restfulHandler._codecs.put(codec, Mock(IStringCodec) {
+            1 * decode(_, _)
+        })
+        restfulHandler._restSvcs.put(svcId, svc)
+        restfulHandler._typeMapper = Mock(TypeMapper) {
+            getType(_) >> String.class
+        }
+
+        then:
+        restfulHandler.put(request, response)
+
+        where:
+        host        | port  | codec     | svcId     | paramName | paramValue    | result    | uriParam  | headerParamName   | headerParamValue
+        'localhost' | 8080  | 'JSON'    | 'hello'   | 'title'   | 'Mr'          | 'Hello'   | 'aaa'     | 'headerName'      | 'test'
+    }
+
+    def 'Test multiple parameter types'() {
+        given:
+        def svc = Mock(IRestfulService) {
+            getMethodArgumentsInfo(HttpMethod.POST) >> [Mock(NamedArgumentMapping) {
+                getName() >> paramName
+                getFrom() >> ArgumentFrom.Param
+                getType() >> Integer.class.getName()
+            }, Mock(IndexedArgumentMapping) {
+                getFrom() >> ArgumentFrom.Uri
+                getType() >> Long.class.getName()
+                getIndex() >> 0
+            }, Mock(NamedArgumentMapping) {
+                getFrom() >> ArgumentFrom.Header
+                getType() >> Boolean.class.getName()
+                getName() >> headerParamName
+            }, Mock(NamedArgumentMapping) {
+                getFrom() >> ArgumentFrom.Header
+                getType() >> Float.class.getName()
+                getName() >> headerParamName1
+            }, Mock(NamedArgumentMapping) {
+                getFrom() >> ArgumentFrom.Header
+                getType() >> Double.class.getName()
+                getName() >> headerParamName2
+            }]
+            1 * invoke(HttpMethod.POST, _ as List) >> result
+        }
+        def headerMap = new HashMap()
+        headerMap.put(headerParamName, headerParamValue)
+        headerMap.put(headerParamName1, headerParamValue1)
+        headerMap.put(headerParamName2, headerParamValue2)
+        def request = Mock(IHttpRequest) {
+            method() >> HttpMethod.POST
+            uri() >> '/rest/' + svcId + '/' + uriParam + '?' + paramName + '=' + paramValue
+            params() >> [ 'title' : [paramValue] as List ]
+            headers() >> headerMap
+        }
+        def response = Mock(IHttpResponse) {
+            1 * write(result)
+            1 * flush()
+        }
+
+        when:
+        RestfulHttpHandler restfulHandler = new RestfulHttpHandler()
+        restfulHandler._host = host
+        restfulHandler._port = port
+        restfulHandler._codecName = codec
+        restfulHandler._codecs.put(codec, Mock(IStringCodec) {
+            1 * decode(_, _) >> result
+        })
+        restfulHandler._restSvcs.put(svcId, svc)
+        restfulHandler._typeMapper = Mock(TypeMapper) {
+            getType(_) >> String.class
+        }
+
+        then:
+        restfulHandler.put(request, response)
+
+        where:
+        host        | port  | codec     | svcId     | paramName | paramValue    | result    | uriParam  | headerParamName   | headerParamValue  | headerParamName1  | headerParamValue1 | headerParamName2  | headerParamValue2
+        'localhost' | 8080  | 'JSON'    | 'hello'   | 'title'   | '1'           | 'Hello'   | '11'      | 'headerName'      | 'true'            | 'headerName1'     | '1.1'             | 'headerName2'     | '1.2'
+
     }
 }
--- uapi.web/src/main/resources/template/getReturnTypeName_method.ftl
+++
@@ -1,6 +0,0 @@
-<#list model?keys as key>
-        if (method == uapi.web.HttpMethod.${key}) {
-            return "${model[key].returnTypeName}";
-        }
-</#list>
-        throw new uapi.KernelException("No method is mapped to http method {}", method);
